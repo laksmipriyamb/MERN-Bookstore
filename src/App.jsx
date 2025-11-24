6614313e84015ea59685eb902daaf28ff2191b9b
lakshmipriyamb <lakshmipriyamb2002@gmail.com>
2025-11-24 12:59:57 +0530

    MERN bookstore created

---
+++ src/App.jsx
@@ -0,0 +1,50 @@
+import { Route, Routes } from 'react-router-dom'
+import './App.css'
+import Home from './users/pages/Home'
+import Books from './users/pages/Books'
+import Profile from './users/pages/Profile'
+import View from './users/pages/View'
+import Contact from './users/pages/Contact'
+import AdminHome from './admin/pages/AdminHome'
+import AdminProfile from './admin/pages/AdminProfile'
+import AdminCollections from './admin/pages/AdminCollection'
+import Auth from './pages/Auth'
+import Pnf from './pages/Pnf'
+import Preloader from './components/Preloader'
+import { useState } from 'react'
+
+function App() {
+
+  const [loading,setLoading] = useState(true)
+
+  setTimeout(() => {
+    setLoading(false)
+  }, 7500);
+
+  return (
+    <>
+      <Routes>
+        <Route path='/' element={loading ? <Preloader/>:<Home/>}/>
+        <Route path='/login' element={<Auth/>}/>
+        <Route path='/register' element={<Auth insideRegister = {true}/>}/>
+        <Route path='/contact' element={<Contact/>}/>
+        <Route path='/books' element={<Books/>}/>
+
+
+        <Route path='/user/profile' element={<Profile/>}/>
+        <Route path='/books/:id/view' element={<View/>}/>
+
+
+        <Route path='/admin/home' element={<AdminHome/>}/>
+        <Route path='/admin/profile' element={<AdminProfile/>}/>
+        <Route path='/admin/resources' element={<AdminCollections/>}/>
+
+
+        <Route path='/*' element={<Pnf/>}/>
+        
+      </Routes>
+    </>
+  )
+}
+
+export default App
