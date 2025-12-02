930c2b8ee13ab8d75a14ec7853c0bd4df2679dcc
lakshmipriyamb <lakshmipriyamb2002@gmail.com>
2025-12-02 10:30:04 +0530

    api call

--- src/pages/Auth.jsx
+++ src/pages/Auth.jsx
@@ -1,37 +1,55 @@
 import React, { useState } from 'react'
 import { FaEye, FaEyeSlash, FaUser } from 'react-icons/fa'
 import { Link } from 'react-router-dom'
+import { ToastContainer, toast } from 'react-toastify';
 
-function Auth({insideRegister}) {
-  const [viewPassword,setViewPassword] = useState(false)
+function Auth({ insideRegister }) {
+  const [viewPassword, setViewPassword] = useState(false)
 
-  
+  // store data from form
+  const [userDetails, setUserDetails] = useState({
+    username: "", email: "", password: ""
+  })
+  //console.log(userDetails);
+
+  const handleRegister = (e) => {
+    e.preventDefault()
+    const { username, email, password } = userDetails
+    if (email && password && username) {
+      toast.success("API Call")
+    } else {
+      toast.info("Please fill the form completely!!!")
+    }
+  }
+
+
+
   return (
     <div className='w-full min-h-screen flex justify-center items-center flex-col bg-[url(/login-bg.png)] bg-cover bg-center'>
       <div className="p-10">
         <h1 className="text-3xl font-bold text-white text-center">BOOK STORE</h1>
-        <div style={{width:'400px',backgroundColor:'rgba(0,0,0,0.9'}} className=" text-white p-5 flex flex-col justify-center items-center my-5">
-          <div style={{width:'100px',height:'100px',borderRadius:'50%'}} className="border mb-5 flex justify-center items-center">
-            <FaUser className='text-3xl'/>
+        <div style={{ width: '400px', backgroundColor: 'rgba(0,0,0,0.9' }} className=" text-white p-5 flex flex-col justify-center items-center my-5">
+          <div style={{ width: '100px', height: '100px', borderRadius: '50%' }} className="border mb-5 flex justify-center items-center">
+            <FaUser className='text-3xl' />
           </div>
-          <h2 className="text-2xl">{insideRegister?"Register":"Login"}</h2>
+          <h2 className="text-2xl">{insideRegister ? "Register" : "Login"}</h2>
           <form className="my-5 w-full">
             {/* username */}
             {
               insideRegister &&
-              <input type="text" placeholder='Username' className='bg-white text-black placeholder-gray-400 w-full p-2 rounded mt-5' />
+              <input value={userDetails.username} onChange={(e) => setUserDetails({ ...userDetails, username: e.target.value })} type="text" placeholder='Username' className='bg-white text-black placeholder-gray-400 w-full p-2 rounded mt-5' />
             }
             {/* email */}
-            <input type="text" placeholder='Email ID' className='bg-white text-black placeholder-gray-400 w-full p-2 rounded my-5' />
+            <input value={userDetails.email} onChange={(e) => setUserDetails({ ...userDetails, email: e.target.value })} type="text" placeholder='Email ID' className='bg-white text-black placeholder-gray-400 w-full p-2 rounded my-5' />
             {/* password */}
-           <div className='flex justify-center items-center'> 
-            <input type={viewPassword?"text":"password"} placeholder='Password' className='bg-white text-black placeholder-gray-400 w-full p-2 rounded mb-5 me-6' />
-            {
-              viewPassword ?
-              <FaEyeSlash onClick={()=>setViewPassword(!viewPassword)} className='text-gray-400 me-2 cursor-pointer' style={{marginLeft:'-50px',marginTop:'-20px'}}/>
-              :
-            <FaEye onClick={()=>setViewPassword(!viewPassword)} className='text-gray-400 me-2 cursor-pointer' style={{marginLeft:'-50px',marginTop:'-20px'}}/>
-            }
+            <div className='flex justify-center items-center'>
+              <input value={userDetails.password} onChange={(e) => setUserDetails({ ...userDetails, password: e.target.value })} type={viewPassword ? "text" : "password"} placeholder='Password' className='bg-white text-black placeholder-gray-400 w-full p-2 rounded mb-5 me-6' />
+              {
+                viewPassword ?
+                  <FaEyeSlash onClick={() => setViewPassword(!viewPassword)} className='text-gray-400 me-2 cursor-pointer' style={{ marginLeft: '-50px', marginTop: '-20px' }} />
+                  :
+                  <FaEye onClick={() => setViewPassword(!viewPassword)} className='text-gray-400 me-2 cursor-pointer' style={{ marginLeft: '-50px', marginTop: '-20px' }} />
+              }
             </div>
             {/* forgot password */}
             {
@@ -44,25 +62,27 @@ function Auth({insideRegister}) {
             {/* login/register btn */}
             <div className='text-center'>
               {
-                insideRegister?
-                <button className='bg-green-700 p-2 w-full rounded'>Register</button>
-                :
-                <button className='bg-green-700 p-2 w-full rounded'>Login</button>
+                insideRegister ?
+                  <button onClick={handleRegister} className='bg-green-700 p-2 w-full rounded'>Register</button>
+                  :
+                  <button className='bg-green-700 p-2 w-full rounded'>Login</button>
               }
             </div>
             {/* already a user  */}
             <div className='text-center'>
               {
-                insideRegister?
-                <p className="text-blue-600 mt-4">Already a user? <Link to={'/login'} className='underline ms-5'>Login</Link></p>
-                :
-                <p className="text-blue-600 mt-4">Are you a new user? <Link to={'/register'} className='underline ms-5'>Register</Link></p>
+                insideRegister ?
+                  <p className="text-blue-600 mt-4">Already a user? <Link to={'/login'} className='underline ms-5'>Login</Link></p>
+                  :
+                  <p className="text-blue-600 mt-4">Are you a new user? <Link to={'/register'} className='underline ms-5'>Register</Link></p>
               }
             </div>
           </form>
 
         </div>
       </div>
+      {/* toast */}
+      <ToastContainer position="top-center" autoClose={3000} theme="colored" />
     </div>
   )
 }
---
+++ src/services/allAPI.js
@@ -0,0 +1,7 @@
+import commonAPI from "./commonAPI"
+import serverURL from "./serverURL"
+
+//register api:called by Auth component when register btn clicked
+export const registerAPI = async (userDetails)=>{
+    return await commonAPI("POST",`${serverURL}/register`,userDetails)
+}
