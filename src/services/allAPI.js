258e55bf6bd9fe9dc03ac4ed628c01dd0f112fce
lakshmipriyamb <lakshmipriyamb2002@gmail.com>
2025-12-16 13:08:05 +0530

    added several books

--- src/services/allAPI.js
+++ src/services/allAPI.js
@@ -11,6 +11,12 @@ export const loginAPI = async (userDetails)=>{
     return await commonAPI("POST",`${serverURL}/login`,userDetails)
 }
 
+//google/sign-in google login api : called by auth when login using google btn clicked 
 export const googleLoginAPI = async (userDetails)=>{
     return await commonAPI("POST",`${serverURL}/google/sign-in`,userDetails)
+}
+
+///user/book/add - addbook api : called by sellerBook component when add book btn click
+export const addBookAPI = async (reqBody,reqHeader)=>{
+    return await commonAPI("POST",`${serverURL}/user/book/add`,reqBody,reqHeader)
 }
